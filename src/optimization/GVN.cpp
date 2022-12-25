671b71dcb10386f74fa3d29952532f2d6a877c06
Wisgy <sunc_y_@mail.ustc.edu.cn>
2022-12-25 15:53:35 +0800

    1suc?

--- src/optimization/GVN.cpp
+++ src/optimization/GVN.cpp
@@ -136,7 +136,7 @@ GVN::partitions GVN::join(const partitions &P1, const partitions &P2) {
     if((*P2.begin())->index_==0)
         return P1;
     partitions P;
-    if(P1==P2)std::cout<<"Really surprise your mother fucker!"<<std::endl;
+    // if(P1==P2)std::cout<<"Really surprise your mother fucker!"<<std::endl;
     for(auto &Ci : P1)
         for(auto &Cj : P2){
             auto Ck = intersect(Ci, Cj);
@@ -205,7 +205,7 @@ void GVN::detectEquivalences() {
             // and the phi instruction in all the successors
             auto pre_bb1 = pre_bb.begin();
             auto pre_bb2 = pre_bb.begin();
-            std::cout<<pre_bb.size()<<std::endl;
+            // std::cout<<pre_bb.size()<<std::endl;
             if(pre_bb.size()==2){
                 pre_bb2++;
                 pout = join(pout_[*pre_bb1], pout_[*pre_bb2]);
@@ -240,7 +240,7 @@ void GVN::detectEquivalences() {
             if(!(pout_[bb] == pout)){
                 pout_[bb] = pout;
                 changed = true;
-            }   
+            }
         }
     } while (changed);
 }
@@ -252,7 +252,7 @@ shared_ptr<Expression> GVN::valueExpr(Instruction *instr, partitions& pin) {
     auto op_num=instr->get_num_operand();
     std::shared_ptr<Expression> lhs,rhs;
     bool flag=true;
-    std::cout<<"op_num="<<op_num<<std::endl;
+    // std::cout<<"op_num="<<op_num<<std::endl;
     if(instr->is_call()){
         if(func_info_->is_pure_function(static_cast<Function*>(instr->get_operand(0)))){
             for(auto &C : pin){
@@ -344,7 +344,7 @@ GVN::partitions GVN::transferFunction(Instruction *x, partitions pin) {
             if(Ci->value_expr_==ve || (vpf!=nullptr&&Ci->value_phi_==vpf)){
                 Ci->members_.insert(x);
                 // Ci->value_expr_=ve;
-                std::cout<<"must"<<std::endl;
+                // std::cout<<"must"<<std::endl;
                 flag=false;
                 break;
             }
@@ -549,7 +549,7 @@ bool operator==(const GVN::partitions &p1, const GVN::partitions &p2) {
 bool CongruenceClass::operator==(const CongruenceClass &other) const {
     // TODO: which fields need to be compared?
     if(this->members_.size()!=other.members_.size())return false;
-    if(this->value_phi_!=other.value_phi_)return false;
+    if(!(this->value_phi_==other.value_phi_))return false;
     for(auto &mem : other.members_){
         if(this->members_.count(mem))continue;
         else return false;
