b640f118a6fd4dab698aee8ca10b4797d854fa5c
Wisgy <sunc_y_@mail.ustc.edu.cn>
2023-01-24 21:36:19 +0800

    first try

--- include/codegen.hpp
+++ include/codegen.hpp
@@ -6,7 +6,35 @@
 
 using std::string;
 using std::vector;
+using std::set;
 
+class Reg {
+  public:
+    Reg(int index) : id(index) {}
+
+    int id;
+    set<Value*> value;
+    string print() {
+        if (id == 0)
+            return "$zero";
+        if (id == 1)
+            return "$ra";
+        if (id == 2)
+            return "$tp";
+        if (id == 3)
+            return "$sp";
+        if (4 <= id and id <= 11)
+            return "$a" + std::to_string(id - 4);
+        if (12 <= id and id <= 20)
+            return "$t" + std::to_string(id - 12);
+        if (id == 22)
+            return "$fp";
+        assert(false);
+    }
+    bool operator<(Reg& other){
+        return this->id < other.id;
+    }
+};
 class CodeGen {
   public:
     CodeGen(Module *module) : m(module) {}
@@ -22,10 +50,32 @@ class CodeGen {
     }
 
     void run();
+    void visit(BasicBlock* bb);
+    void ret_assembly(Instruction* instr);
+    Reg* GetReg(Instruction* instr);
 
   private:
     Module *m;
     vector<string> output;
 };
+
+class AddrDesc{
+    public:
+        AddrDesc() = default;
+        AddrDesc(Value* value) : val_(value) {}
+        AddrDesc(Value* value, Reg* reg) : val_(value) {reg_.insert(reg);}
+        set<Reg*>& get_regs() { return reg_;}
+        Reg* get_reg() { return reg_.size()?*reg_.begin():NULL;}
+        Value* get_pos() { return pos_.size()?*pos_.begin():NULL;}
+        void push_reg(Reg* reg) {reg_.insert(reg);}
+        void push_addr(Value* addr) {pos_.insert(addr);}
+
+    private:
+        Value* val_;
+        set<Reg*> reg_ ;
+        set<Value*> pos_;
+};
+Reg* RandomReg();
+
 
 #endif
--- src/codegen/codegen.cpp
+++ src/codegen/codegen.cpp
@@ -11,48 +11,85 @@
 // $r22         $fp         frame pointer
 // $r23 - $r31  $s0 - $s8   static
 
-class Reg {
-  public:
-    Reg(int index) : id(index) {}
-
-    int id;
-
-    string print() {
-        if (id == 0)
-            return "$zero";
-        if (id == 1)
-            return "$ra";
-        if (id == 2)
-            return "$tp";
-        if (id == 3)
-            return "$sp";
-        if (4 <= id and id <= 11)
-            return "$a" + std::to_string(id - 4);
-        if (12 <= id and id <= 20)
-            return "$t" + std::to_string(id - 12);
-        if (id == 22)
-            return "$fp";
-        assert(false);
-    }
-};
-
+int next_reg=0;
+std::map<Instruction*, AddrDesc> InstrPos;
+Reg R[32]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
 void CodeGen::run() {
     // TODO: implement
     // 以下内容生成 int main() { return 0; } 的汇编代码
 
     output.push_back(".text");
+    // global_value
+    for (auto& g_v : m->get_global_variable()){
+        // TODO: global_value
+        output.push_back(".comm " + g_v.get_name() + ", 4");
+    }
     for (auto &func : m->get_functions())
         if (not func.is_declaration()) {
+            // entry
             output.push_back(".globl " + func.get_name());
             output.push_back(".type " + func.get_name() + ", @function");
             output.push_back(func.get_name() + ":");
             output.push_back("addi.d $sp, $sp, -16");
             output.push_back("st.d $ra, $sp, 8");
             output.push_back("addi.d $fp, $sp, 16");
-
-            output.push_back("addi.w $a0, $zero, 0");
+            // body
+            for(auto& bb : func.get_basic_blocks()){
+                visit(&bb);
+            }
+            // return
             output.push_back("ld.d $ra, $sp, 8");
             output.push_back("addi.d $sp, $sp, 16");
             output.push_back("jr $ra");
         }
+}
+
+void CodeGen::visit(BasicBlock* bb){
+    output.push_back(bb->get_name() + ":");
+    for(auto& instr : bb->get_instructions()){
+        switch (instr.get_instr_type()){
+            case Instruction::ret: ret_assembly(&instr); break;
+            // case Instruction::br: return "br"; break;
+            // case Instruction::add: return "add"; break;
+            // case Instruction::sub: return "sub"; break;
+            // case Instruction::mul: return "mul"; break;
+            // case Instruction::sdiv: return "sdiv"; break;
+            // case Instruction::fadd: return "fadd"; break;
+            // case Instruction::fsub: return "fsub"; break;
+            // case Instruction::fmul: return "fmul"; break;
+            // case Instruction::fdiv: return "fdiv"; break;
+            // case Instruction::alloca: return "alloca"; break;
+            // case Instruction::load: return "load"; break;
+            // case Instruction::store: return "store"; break;
+            // case Instruction::cmp: return "cmp"; break;
+            // case Instruction::fcmp: return "fcmp"; break;
+            // case Instruction::phi: return "phi"; break;
+            // case Instruction::call: return "call"; break;
+            // case Instruction::getelementptr: return "getelementptr"; break;
+            // case Instruction::zext: return "zext"; break;
+            // case Instruction::fptosi: return "fptosi"; break;
+            // case Instruction::sitofp: return "sitofp"; break;
+            default:
+                assert(false);
+        }
+    }
+}
+Reg* CodeGen::GetReg(Instruction* instr){
+    // if(InstrPos.find(instr)!=InstrPos.end())
+    //     if(InstrPos[instr].get_reg()!=NULL)//存在寄存器含有此变量值
+    //         return InstrPos[instr].get_reg();
+    //     else if(InstrPos[instr].get_pos()!=NULL){//变量值存储在memory中
+    //         auto addr_reg = GetReg(static_cast<Instruction*> (InstrPos[instr].get_pos()));
+    //         // output.push_back("ld.w " + )
+    //     }
+    return RandomReg();
+
+}
+Reg* RandomReg(){
+    next_reg = (next_reg+1)%9;
+    return &R[next_reg+12];
+}
+void CodeGen::ret_assembly(Instruction* instr){
+    auto reg = GetReg(instr);
+    output.push_back("addi.w $a0, " + reg->print() + ", 0");
 }
